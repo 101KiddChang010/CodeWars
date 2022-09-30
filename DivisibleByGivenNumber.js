823625a07d06128d29386bc9dc9c68ae33d53c9c
Kidd <91109377+101KiddChang010@users.noreply.github.com>
2022-09-29 21:41:46 -0400

    Create DivisibleByGivenNumber.js

---
+++ DivisibleByGivenNumber.js
@@ -0,0 +1,21 @@
+/*
+Complete the function which takes two arguments and returns 
+all numbers which are divisible by the given divisor. First 
+argument is an array of numbers and the second is the divisor.
+
+Example(Input1, Input2 --> Output)
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+*/
+
+function divisibleBy(numbers, divisor){
+  const solution = [];
+  for (key in numbers) {
+    let t = numbers[key] % divisor;
+    if (t === 0) {
+      solution.push(numbers[key]);
+    } else if (t != 0) {
+      
+    }
+  }
+  return solution
+}
